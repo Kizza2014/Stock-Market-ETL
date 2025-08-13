1b13c84d518a295d2bf14b72aa91e984b925164b
Kizza2014 <hoangkimgiap_t67@hus.edu.vn>
2025-08-13 18:39:03 +0700

    update requirements, test  standalone spark

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-import pandas as pd
-import os
+from pyspark.sql import SparkSession
 
-path = './extract/data_test/crawl_financials/crawled_on_2025_08_02/cash_flow'
-files = os.listdir(path)
-for filename in files:
-    # Đọc file CSV
-    if filename.endswith('.csv'):
-        df = pd.read_csv(os.path.join(path, filename))
+spark = SparkSession.builder \
+    .appName("TransformCompany") \
+    .master("local[*]") \
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk:1.12.262") \
+    .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
+    .config("spark.hadoop.fs.s3a.secret.key", "minioadmin123") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .getOrCreate()
 
-        # Lưu dưới dạng Parquet
-        new_filename = filename.split(".")[0] + ".parquet"
-        df.to_parquet(os.path.join(path, new_filename), index=False)
+df = spark.read.parquet("s3a://bronze/type=active_tickers/date=2025_08_02/most_active_quotes_parsed.parquet")
+df.show()
